c883b05c14e34bab99c9efcd256c482c02fdb9b1
Codingmasterfred <fredrickg777@gmail.com>
2023-07-16 10:38:22 -0700

    done

--- c-sharp/CodeChallenge5/LinkedList.cs
+++ c-sharp/CodeChallenge5/LinkedList.cs
@@ -1,22 +1,56 @@
-namespace CodeChallenges 
+using System;
+
+public class LinkedList
 {
-    public class LinkedList
+  public Node Head;
+
+  public void AddNewNode(Node node)
+  {
+    node.Next = Head;
+    Head = node;
+  }
+
+  public bool Includes(int value)
+  {
+    Node current = Head;
+    while (current != null)
     {
-        public Node Head;
-        public void AddNewNode(Node node)
-        {
-            node.Next = Head;
-            Head = node;
-        }
+      if (current.Value == value)
+      {
+        return true;
+      }
+      current = current.Next;
+    }
+    return false;
+  }
+
+  public override string ToString()
+  {
+    if (Head == null)
+    {
+      return "NULL";
     }
 
-    public class Node
+    string result = "";
+    Node current = Head;
+    while (current != null)
     {
-        public Node? Next;
-        public int Value;
-        public Node(int value)
-        {
-            Value = value;
-        }
+      result += $"{{ {current.Value} }} -> ";
+      current = current.Next;
     }
+    result += "NULL";
+    return result;
+  }
+}
+
+public class Node
+{
+  public Node Next;
+  public int Value;
+
+  public Node(int value)
+  {
+    Value = value;
+    Next = null;
+  }
 }
--- c-sharp/CodeChallenge5/LinkedListTest.cs
+++ c-sharp/CodeChallenge5/LinkedListTest.cs
@@ -1,24 +1,27 @@
-using CodeChallenges;
 using Xunit;
-using System.Reflection;
-namespace CodeChallengeTests
+
+namespace CodeChallenges.Tests
 {
-  public class CodeChallenge5Test
+  public class LinkedListTests
   {
     [Fact]
-    public void LinkedList_Can_A_Single_Node()
+    public void LinkedList_Can_Add_Single_Node()
     {
+      // Arrange
       LinkedList list = new LinkedList();
       Node node = new Node(5);
 
+      // Act
       list.AddNewNode(node);
-      Assert.Equal(list.Head, node);
 
+      // Assert
+      Assert.Equal(list.Head, node);
     }
 
     [Fact]
-    public void LinkedList_Can_A_Two_Nodes()
+    public void LinkedList_Can_Add_Two_Nodes()
     {
+      // Arrange
       LinkedList list = new LinkedList();
       Node node1 = new Node(5);
       Node node2 = new Node(6);
@@ -27,13 +30,13 @@ namespace CodeChallengeTests
       node2.Next = node1;
       expectedList.Head = node2;
 
-
+      // Act
       list.AddNewNode(node1);
       list.AddNewNode(node2);
+
+      // Assert
       Assert.Equal(expectedList.Head, list.Head);
       Assert.Equal(expectedList.Head.Next, list.Head.Next);
-      
     }
-
   }
 }
