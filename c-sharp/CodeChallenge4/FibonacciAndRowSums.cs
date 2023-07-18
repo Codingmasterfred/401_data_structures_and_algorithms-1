0bc2857b9bbbfdc3af6d18c6b993ddcda11abb83
Codingmasterfred <fredrickg777@gmail.com>
2023-07-17 18:37:27 -0700

    done

--- c-sharp/CodeChallenge4/FibonacciAndRowSums.cs
+++ c-sharp/CodeChallenge4/FibonacciAndRowSums.cs
@@ -1,19 +1,49 @@
-namespace CodeChallenges 
+namespace CodeChallenges
 {
-    public class CodeChallenge4
+  public class CodeChallenge4
+  {
+    public static int GetNthNumberInFibonacciSequence(int n)
     {
-        public static int GetNthNumberInFibonacciSequence(int nthNumberInSequence)
-        {
-            int result = 0;
+      if (n < 0)
+      {
+        throw new ArgumentException("n must be a non-negative integer.");
+      }
 
-            return result;
-        }
+      if (n == 0)
+      {
+        return 0;
+      }
 
-        public static int[] RowSums(int[][] matrix)
-        {
-            int[] rowSums = new int[matrix[0].Length];
-            return rowSums;
-        }
+      int a = 0;
+      int b = 1;
 
+      for (int i = 2; i <= n; i++)
+      {
+        int temp = a + b;
+        a = b;
+        b = temp;
+      }
+
+      return a;
     }
+
+
+    public static int[] RowSums(int[][] matrix)
+    {
+      int rows = matrix.Length;
+      int[] rowSums = new int[rows];
+
+      for (int i = 0; i < rows; i++)
+      {
+        int sum = 0;
+        for (int j = 0; j < matrix[i].Length; j++)
+        {
+          sum += matrix[i][j] != null ? matrix[i][j] : 0;
+        }
+        rowSums[i] = sum;
+      }
+
+      return rowSums;
+    }
+  }
 }
