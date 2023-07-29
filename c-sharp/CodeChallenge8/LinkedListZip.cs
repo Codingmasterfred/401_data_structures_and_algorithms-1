17aaac2e3b5abd73b2c99888ff3574711f9a09d4
Codingmasterfred <fredrickg777@gmail.com>
2023-07-29 16:28:34 -0700

    done with 8

--- c-sharp/CodeChallenge8/LinkedListZip.cs
+++ c-sharp/CodeChallenge8/LinkedListZip.cs
@@ -1,75 +1,32 @@
-using System;
 using System.Collections.Generic;
-
 namespace CodeChallenges
 {
-  public class Node<T>
-  {
-    public T Value { get; set; }
-    public Node<T> Next { get; set; }
-
-    public Node(T value)
-    {
-      Value = value;
-      Next = null;
-    }
-  }
-
-  public class LinkedList<T>
-  {
-    public Node<T> Head { get; set; }
-
-    public LinkedList()
-    {
-      Head = null;
-    }
-
-    // Other methods for adding nodes to the linked list can be implemented here
-  }
-
   public class CodeChallenge8
   {
-    public LinkedList<T> ZipLists<T>(LinkedList<T> list1, LinkedList<T> list2)
+    public static LinkedListKth ZipLists(LinkedListKth list1, LinkedListKth list2)
     {
-      LinkedList<T> resultList = new LinkedList<T>();
-      Node<T> current1 = list1.Head;
-      Node<T> current2 = list2.Head;
-      Node<T> currentResult = null;
+      LinkedListKth result = new LinkedListKth();
+      Node currentNode1 = list1.Head;
+      Node currentNode2 = list2.Head;
 
-      while (current1 != null || current2 != null)
+      while (currentNode1 != null || currentNode2 != null)
       {
-        if (current1 != null)
+        if (currentNode1 != null)
         {
-          if (currentResult == null)
-          {
-            resultList.Head = new Node<T>(current1.Value);
-            currentResult = resultList.Head;
-          }
-          else
-          {
-            currentResult.Next = new Node<T>(current1.Value);
-            currentResult = currentResult.Next;
-          }
-          current1 = current1.Next;
+          result.Append(currentNode1.Value);
+          currentNode1 = currentNode1.Next;
         }
 
-        if (current2 != null)
+        if (currentNode2 != null)
         {
-          if (currentResult == null)
-          {
-            resultList.Head = new Node<T>(current2.Value);
-            currentResult = resultList.Head;
-          }
-          else
-          {
-            currentResult.Next = new Node<T>(current2.Value);
-            currentResult = currentResult.Next;
-          }
-          current2 = current2.Next;
+          result.Append(currentNode2.Value);
+          currentNode2 = currentNode2.Next;
         }
       }
 
-      return resultList;
+
+
+      return result;
     }
   }
 }
